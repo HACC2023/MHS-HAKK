1ad0b10ed81e53bf29489bb9eb4759d2b5301f91
kaizukaaizumi <111713120+kaizukaaizumi@users.noreply.github.com>
2023-10-27 03:54:32 -1000

    Update index.tsx

--- src/components/LeafletMap/index.tsx
+++ src/components/LeafletMap/index.tsx
@@ -1,18 +1,17 @@
 import React, { useRef, useEffect, useState } from "react";
-import L from "leaflet";
+import L, { IconOptions } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import geoJsonData from './geoJSON.json';
 
-
-delete L.Icon.Default.prototype._getIconUrl;
+delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
     iconUrl: markerIcon.src,
     iconRetinaUrl: markerIcon2x.src,
     shadowUrl: markerShadow.src,
-})
+});
 
 const LeafletMap: React.FC = () => {
     const mapRef = useRef<HTMLDivElement>(null);
