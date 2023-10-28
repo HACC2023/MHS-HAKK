50f01e1461ab0c4d72be0ae033862f075dcfeb60
jhung-mililani <115854825+jhung-mililani@users.noreply.github.com>
2023-10-28 02:32:21 -1000

    expand search, purge unused css on publish

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,15 +19,27 @@ export default function Home() {
             </label>
             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
               <li><a>Homepage</a></li>
-              <li><a>Portfolio</a></li>
+              <li><a>Health Centers</a></li>
+              <li><a>Insurance Price Calculator</a></li>
               <li><a>About</a></li>
             </ul>
           </div>
         </div>
         <div className="navbar-center">
           {/* search */}
-          <div className="form-control">
-            <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+          <div className="join">
+            <select className="select select-bordered join-item">
+              <option disabled selected>Filter</option>
+              <option>Federally Qualified Health Centers</option>
+              <option>Rural Health Centers</option>
+              <option>QUEST Providers</option>
+            </select>
+            <div>
+              <div>
+                <input className="input input-bordered join-item" placeholder="Search Provider" />
+              </div>
+            </div>
+              <button className="btn btn-success btn-outline btn-circle join-item"><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg></button>
           </div>
         </div>
         <div className="navbar-end">
