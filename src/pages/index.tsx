fda1e4735e8e1b898af6252c0860c7c9043b75d7
jhung-mililani <115854825+jhung-mililani@users.noreply.github.com>
2023-10-27 22:36:33 -1000

    format cmts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import "leaflet/dist/leaflet.css";
-
 
 export default function Home() {
 
@@ -14,7 +12,8 @@ export default function Home() {
       </Head>
       <div className="navbar bg-base-100">
         <div className="navbar-start">
-          <div className="dropdown"> {/* menu */}
+          <div className="dropdown"> 
+            {/* menu */}
             <label tabIndex={0} className="btn btn-ghost btn-circle">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
             </label>
@@ -26,12 +25,14 @@ export default function Home() {
           </div>
         </div>
         <div className="navbar-center">
-          <div className="form-control"> {/* search */}
+          {/* search */}
+          <div className="form-control">
             <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
           </div>
         </div>
         <div className="navbar-end">
-          <a className="btn btn-ghost normal-case text-xl" href="https://github.com/jhung-mililani/MHSHAKK">MHS-HAKK</a> {/* link */}
+          {/* link */}
+          <a className="btn btn-ghost normal-case text-xl" href="https://github.com/jhung-mililani/MHSHAKK">MHS-HAKK</a>
         </div>
       </div>
     </>
