0ce34c43455a3b40c0a3befe387060805aad49cb
jhung-mililani <115854825+jhung-mililani@users.noreply.github.com>
2023-10-27 23:17:02 -1000

    add copyright

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       </Head>
       <div className="navbar bg-base-100">
         <div className="navbar-start">
-          <div className="dropdown"> 
-            {/* menu */}
+          {/* menu */}
+          <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost btn-circle">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
             </label>
@@ -35,6 +35,12 @@ export default function Home() {
           <a className="btn btn-ghost normal-case text-xl" href="https://github.com/jhung-mililani/MHSHAKK">MHS-HAKK</a>
         </div>
       </div>
+      <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+        {/* copyright */}
+        <aside>
+          <p>© 2023 <a href="https://www.mililanihs.org/" style={{ textDecoration: 'underline' }}>Mililani High School.</a> All Rights Reserved.</p>
+        </aside>
+      </footer>
     </>
   );
 } 
