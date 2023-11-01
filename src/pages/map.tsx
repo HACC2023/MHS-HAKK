a4272dc5ba6fe769fb800380f2636354a4b561ea
jhung <115854825+jhung-mililani@users.noreply.github.com>
2023-10-31 17:31:56 -1000

    import mapping pages

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -5,7 +5,7 @@ import "leaflet/dist/leaflet.css";
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
-import { api } from "../../utils/api";
+import { api } from "../utils/api";
 import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
 
 
