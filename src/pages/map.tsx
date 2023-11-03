92d6b6ea2eedaa56a90c31f662c22442dc63b5a2
jhung <115854825+jhung-mililani@users.noreply.github.com>
2023-11-02 14:10:42 -1000

    add nav/footer and TODO to map

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
 import "leaflet/dist/leaflet.css";
+import Navbar from '~/components/navbar';
+import Footer from '~/components/footer';
 
 const LeafletMap = dynamic(() => import('~/components/mapCreation'), { ssr: false });
 
 const mapPage: React.FC = () => {
+    {/**@todo wrap mapKey in a react component */}
     const [mapKey] = useState(0);
     return (
-        <div>
-            <LeafletMap key={mapKey}/>
-        </div>
+        <>
+            <Navbar></Navbar>
+            <div>
+                <LeafletMap key={mapKey} />
+            </div>
+            <Footer></Footer>
+        </>
     )
 }
 
