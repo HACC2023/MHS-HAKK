f8b61b8672fde69e304a678e1e773f56017398a6
CourtneyHisa <courtneyhisamoto@gmail.com>
2023-11-08 16:55:41 -1000

    Updated MedQuest Eligibility Link

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,13 +1,9 @@
 // import { useState } from "react";
 // import SearchBarAutocomplete from "~/components/SearchBar/AutoComplete";
-import Image from "next/image";
 import Navbar from "~/components/Navbar";
 import Footer from "~/components/Footer";
 
 export default function searchPage() {
-
-    // const [price, setPrice] = useState("");
-
     return (
         <>
             <Navbar />
@@ -35,11 +31,11 @@ export default function searchPage() {
                         <h1 className="text-3xl  font-semibold pt-8">What is Med-QUEST?</h1>
                         <p className="text-xl  font-medium pt-4">Med-QUEST is a government health insurance plan for the uninsured. QUEST covers a wide variety of healthcare services, so check if you may qualify for these benefits by checking</p>
                         <div className="flex justify-center">
-                            <a className="normal-case btn  text-lg font-semibold mt-6 h-14 bg-light-green hover:bg-hover-green text-green-gray border-0" target="_blank" href="https://humanservices.hawaii.gov/mqd/quest-overview/">Med-QUEST Eligibilty</a>
+                            <a className="normal-case btn  text-lg font-semibold mt-6 h-14 bg-light-green hover:bg-hover-green text-green-gray border-0" target="_blank" href="https://medquest.hawaii.gov/en/contact-us.html">Med-QUEST Eligibilty</a>
                         </div>
                     </div>
                     <div className="w-1/2">
-                        <Image className="h-full w-3/4 object-cover" alt="Image of Hawaii" src="https://images.unsplash.com/photo-1591068929753-4d1cf8fc6ff7?auto=format&fit=crop&q=80&w=1932&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></Image>
+                        <img className="h-full w-3/4 object-cover" alt="Image of Hawaii" src="https://images.unsplash.com/photo-1591068929753-4d1cf8fc6ff7?auto=format&fit=crop&q=80&w=1932&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
                     </div>
                 </div>
             </div>
