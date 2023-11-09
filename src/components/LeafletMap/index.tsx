d31e2c4d7844d607cd5be667a41027172c2ff6cb
kaizukaaizumi <111713120+kaizukaaizumi@users.noreply.github.com>
2023-11-09 02:26:32 -1000

    11/9 sleep

--- src/components/LeafletMap/index.tsx
+++ src/components/LeafletMap/index.tsx
@@ -1,25 +1,20 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import L, { type LocationEvent, type ErrorEvent } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import * as jsonData from './Coordinates.json';
-type ModdedGSONObj = (GeoJSON.GeoJsonObject & { properties: { address: string } });
-const data: ModdedGSONObj[] = jsonData.map((c) => {
-    return {
-        type: "Feature",
-        geometry: {
-            type: "Point",
-            coordinates: [
-                c.Longitude,
-                c.Latitude
-            ],
-        },
-        properties: {
-            address: c.address
-        },
-    };
+import { api } from "~/utils/api";
+type ModdedGSONObj = (GeoJSON.GeoJsonObject & {
+    properties: {
+        id: string;
+        address: string;
+        names: string[];
+        website: string | null;
+        healthCenterNumbers: string[];
+        insurancePlans: string[];
+    }
 });
 
 L.Icon.Default.mergeOptions({
@@ -28,63 +23,99 @@ L.Icon.Default.mergeOptions({
     shadowUrl: markerShadow.src,
 });
 
-const LeafletMap = () => {
+const LeafletMap: React.FC = () => {
     const mapRef = useRef<HTMLDivElement>(null);
+    const queryData = api.healthcare.getSome.useQuery().data;
+    const [bruh, setBruh] = useState<ModdedGSONObj[]>([]);
 
     useEffect(() => {
-        const initializeMap = () => {
-            try {
-                if (mapRef.current) {
-                    const geoJSONLayer = L.geoJSON(data, {
-                        pointToLayer: function (feature: ModdedGSONObj, latlng) {
-                            const address = feature.properties.address;
-                            const marker = L.marker(latlng, {
-                                title: address,
-                            });
+        if (!queryData) return;
 
-                            marker.bindPopup(address);
+        const properties = queryData.map<ModdedGSONObj | null>((x, index) => {
+            const c = jsonData[index];
+            if (!c) return null;
 
-                            return marker;
-                        },
-                        coordsToLatLng: function (coords) {
-                            return new L.LatLng(coords[1], coords[0], coords[2]);
-                        }
-                    });
+            const { address, id, names, healthCenterNumbers, insurancePlans } = x;
+            return {
+                type: "Feature",
+                geometry: {
+                    type: "Point",
+                    coordinates: [c.Longitude, c.Latitude],
+                },
+                properties: { id, address, names, website: x.website, healthCenterNumbers, insurancePlans },
+            };
+        }).filter(Boolean) as ModdedGSONObj[];
 
+        setBruh(properties);
+    }, [queryData]);
 
-                    const map = L.map(mapRef.current).setView([21.306944, -157.858337], 13);
-                    geoJSONLayer.addTo(map);
-                    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-                        attribution:
-                            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-                        maxZoom: 18,
-                    }).addTo(map);
+    useEffect(() => {
+        if (bruh.length > 0 && mapRef.current) {
+            initializeMap();
+        }
+    }, [bruh]);
 
-                    function onLocationFound(e: LocationEvent) {
-                        const radius = e.accuracy;
+    const initializeMap = () => {
+        try {
+            if (mapRef.current && bruh.length > 0) {
+                console.warn('mapRef.current && bruh.length > 0 === true')
+                const geoJSONLayer = L.geoJSON(bruh, {
+                    pointToLayer: function (feature: ModdedGSONObj, latlng) {
+                        const { address, id, names, healthCenterNumbers, insurancePlans, website } = feature.properties;
+                        console.log(address, id, names, healthCenterNumbers, insurancePlans)
+                        const marker = L.marker(latlng, {
+                            title: address,
+                        });
+                        const formattedHealthCenterNumbers = healthCenterNumbers.map((number) => {
+                            return number.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
+                        });
+                        const websiteLink = website ? `<p><strong>Website:</strong> <a href="${website}" target="_blank">${website}</a></p>` : '';
+                        const popupContent = `
+                        <div>
+                            <h2>${names.join(', ')}</h2>
+                            <p><strong>Address:</strong> ${address}</p>
+                            <p><strong>Health Center Numbers:</strong> ${formattedHealthCenterNumbers.join(', ')}</p>
+                            <p><strong>Insurance Plans:</strong> ${insurancePlans.join(', ')}</p>
+                            ${websiteLink}
+                        </div>
+                        `;
 
-                        L.marker(e.latlng).addTo(map)
-                            .bindPopup("You are within " + radius + " meters from this point").openPopup();
-
-                        L.circle(e.latlng, radius).addTo(map);
+                        marker.bindPopup(popupContent);
+                        return marker;
                     }
+                });
+                const map = L.map(mapRef.current).setView([21.306944, -157.858337], 12);
+                geoJSONLayer.addTo(map);
+                L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+                    attribution:
+                        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+                    maxZoom: 18,
+                }).addTo(map);
 
-                    map.on('locationfound', onLocationFound);
+                function onLocationFound(e: LocationEvent) {
+                    const radius = e.accuracy;
 
-                    function onLocationError(e: ErrorEvent) {
-                        alert(e.message);
-                    }
+                    L.marker(e.latlng).addTo(map)
+                        .bindPopup("You are within " + radius + " meters from this point").openPopup();
 
-                    map.on('locationerror', onLocationError);
-
+                    L.circle(e.latlng, radius).addTo(map);
                 }
-            } catch (error) {
-                console.log("Error initializing map:", error);
-            }
-        };
 
-        initializeMap();
-    }, []);
+                map.on('locationfound', onLocationFound);
+
+                function onLocationError(e: ErrorEvent) {
+                    alert(e.message);
+                }
+
+                map.on('locationerror', onLocationError);
+
+            } else {
+                console.warn('mapRef.current && bruh.length > 0 === false')
+            }
+        } catch (error) {
+            console.error("Error initializing map:", error);
+        }
+    };
 
     return <div ref={mapRef} className="w-2/5 h-full ml-auto"></div>;
 };
