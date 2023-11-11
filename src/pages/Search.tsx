f1411bc9cb330f711f075ab30803d382378d8ba6
shringo <143767857+shringo@users.noreply.github.com>
2023-11-10 23:11:52 -1000

    FINALIZE SEARCH PAGE UI FOR MOBILE AND FUNCTIONALITY

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import dynamic from "next/dynamic";
 import "leaflet/dist/leaflet.css";
-import type { InsuranceProviders } from "~/server/api/routers/HealthcareRouter";
+import type {
+  InsuranceProviders,
+  ProcedureProviders,
+} from "~/server/api/routers/HealthcareRouter";
 import { api } from "~/utils/api";
 import Navbar from "~/components/Navbar";
 import { LoadingSpinner } from "~/components/Loading";
+import Link from "next/link";
 const LeafletMap = dynamic(() => import("../components/LeafletMap"), {
   ssr: false,
 });
@@ -12,7 +16,8 @@ const LeafletMap = dynamic(() => import("../components/LeafletMap"), {
 const SearchPage: React.FC = () => {
   // this is used for filtering.
   // allow user to set insurance status (QUEST, none, or both)
-  const [insurance, setInsurance] = useState<InsuranceProviders | undefined>();
+  const [insurance, setInsurance] = useState<InsuranceProviders>();
+  const [procedure, setProcedure] = useState<ProcedureProviders>();
 
   const [mobileOnResultsView, setMORV] = useState(true);
 
@@ -20,14 +25,44 @@ const SearchPage: React.FC = () => {
   // if there is, plug it into our api.
   // this really needs to be one procedure with the insurance being optional.
   // this is unsorted. it is sorted however mongodb wants to sort it :)
-  const { data: centers, isLoading } =
-    insurance !== undefined
-      ? api.healthcare.getByPlan.useQuery({ insurance })
-      : api.healthcare.getSome.useQuery();
+  const {
+    data: centers,
+    isLoading,
+    isError,
+  } = api.healthcare.getByPlan.useQuery({ insurance, procedure });
+
+  const InsuranceOptions = <SelectInsurance setInsurance={setInsurance} />;
+  const ProcedureOptions = <SelectProcedure setProcedure={setProcedure} />;
+
+  console.log(centers);
+  const Results =
+    isLoading || !centers?.length || isError ? (
+      <div className="mt-12 grid justify-items-center justify-self-center text-center text-3xl">
+        {isError ? (
+          <p className="mx-4">
+            An internal error occurred. Please try again later.
+          </p>
+        ) : isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          <p className="mx-4">
+            No centers were found. Try adjusting your filters.
+          </p>
+        )}
+      </div>
+    ) : (
+      <ClinicResults centers={centers} />
+    );
 
   return (
-    <div className="h-screen overflow-hidden font-tyler">
+    <div
+      className={
+        "h-screen overflow-x-hidden font-tyler md:block md:overflow-y-auto " +
+        (!mobileOnResultsView && "fixed overflow-y-hidden")
+      }
+    >
       <Navbar />
+      {/* Desktop view */}
       <div className="hidden h-[calc(100%-5rem)] w-screen md:flex">
         <div
           className="0 
@@ -42,7 +77,13 @@ const SearchPage: React.FC = () => {
                 Insurance type
               </span>
             </label>
-            <SelectInsurance setInsurance={setInsurance} />
+            {InsuranceOptions}
+            <label className="label">
+              <span className="label-text text-base font-semibold">
+                Procedure type
+              </span>
+            </label>
+            {ProcedureOptions}
           </div>
         </div>
         <div
@@ -56,60 +97,80 @@ const SearchPage: React.FC = () => {
                     DO NOT USE STATIC SITE GENERATION BECAUSE YOUTUBE DOESN'T DO THIS WHEN YOU SEARCH FOR VIDEOS
                     then, when we get the data we can map it out into a DIV for each center. rn we are missing a more info <button className=""></button>
                     */}
-          {isLoading || !centers ? (
-            <div className="mt-12 grid justify-items-center justify-self-center">
-              {isLoading ? (
-                <LoadingSpinner />
-              ) : (
-                "Centers not found. Try clearing filters?"
-              )}
-            </div>
-          ) : (
-            <ClinicResults centers={centers} />
-          )}
+          {Results}
         </div>
         {/* possibly feed the list of locations into here, it was requested/needed that the leaflet map should only show endpoints
                 that are in the search results, but I bring up the problem of pagination again. */}
         <LeafletMap key={0} />
       </div>
-      <div className="h-[calc(100%-5rem)] w-screen block md:hidden">
-        <div className="pl-5 pt-3">Pick Insurance</div>
-        <div className="ml-auto mr-auto flex w-full space-x-4 p-5 pt-0">
-          <SelectInsurance setInsurance={setInsurance} />
+
+      {/* Mobile view */}
+      <div className="flex h-[calc(100vh-5rem)] w-screen flex-col md:hidden">
+        <dialog id="optionsDialogue" className="modal">
+          <div className="modal-box space-y-4">
+            <form method="dialog">
+              <button className="btn btn-circle btn-ghost btn-sm absolute right-2 top-2">
+                ✕
+              </button>
+            </form>
+            <div>
+              <h3 className="text-lg font-bold">Select Insurance Provider</h3>
+              {InsuranceOptions}
+            </div>
+            <div>
+              <h3 className="text-lg font-bold">Select Procedure Type</h3>
+              {ProcedureOptions}
+            </div>
+          </div>
+          <form method="dialog" className="modal-backdrop">
+            <button />
+          </form>
+        </dialog>
+        <div className="flex max-h-fit pb-1 pl-5 pt-3">
+          Pick Insurance / Procedures
+        </div>
+        <div className="mx-auto flex max-h-fit w-full space-x-4 p-5 pt-0">
+          {/* You can open the modal using document.getElementById('ID').showModal() method */}
+          <button
+            className="select-bordered w-full rounded-lg border border-[hsl(var(--bc)/var(--tw-border-opacity))] p-2 px-2 text-center"
+            onClick={() =>
+              (
+                document.getElementById("optionsDialogue") as HTMLDialogElement
+              ).showModal()
+            }
+          >
+            Click for options
+          </button>
           <div className="join">
             <a
               onClick={() => setMORV(true)}
-              className={(mobileOnResultsView ? "bg-slate-200" : "bg-transparent") + " join-item select-bordered flex flex-col content-center justify-center border border-[hsl(var(--bc)/var(--tw-border-opacity))] px-2 text-center"}
+              className={
+                (mobileOnResultsView
+                  ? "bg-slate-200"
+                  : "flex-grow bg-transparent") +
+                " join-item select-bordered flex flex-col content-center justify-center border border-[hsl(var(--bc)/var(--tw-border-opacity))] p-2 px-2 text-center"
+              }
             >
               Results
             </a>
             <a
               onClick={() => setMORV(false)}
-              className={(!mobileOnResultsView ? "bg-slate-200" : "bg-transparent") + " join-item select-bordered flex flex-col content-center justify-center border border-[hsl(var(--bc)/var(--tw-border-opacity))] px-2 text-center"}
+              className={
+                (!mobileOnResultsView ? "bg-slate-200" : "bg-transparent") +
+                " join-item select-bordered flex flex-col content-center justify-center border border-[hsl(var(--bc)/var(--tw-border-opacity))] p-2 px-2 text-center"
+              }
             >
               Map
             </a>
           </div>
         </div>
-        {mobileOnResultsView ? (
-          <div
-            className="h-[calc(100%-6.5rem)] flex-col overflow-y-scroll"
-          >
-            {isLoading || !centers ? (
-              <div className="mt-12 grid justify-items-center justify-self-center">
-                {isLoading ? (
-                  <LoadingSpinner />
-                ) : (
-                  "Centers not found. Try clearing filters?"
-                )}
-              </div>
-            ) : (
-              <ClinicResults centers={centers} />
-            )}
-          </div>
-        ) : (
-          <LeafletMap key={0} />
-        )}
+        <div
+          className={
+            mobileOnResultsView ? "flex-1" : "h-full flex-1 overflow-y-hidden"
+          }
+        >
+          {mobileOnResultsView ? Results : <LeafletMap key={0} />}
+        </div>
       </div>
     </div>
   );
@@ -118,22 +179,57 @@ const SearchPage: React.FC = () => {
 export default SearchPage;
 
 function SelectInsurance(props: {
-  setInsurance: (
-    value: React.SetStateAction<InsuranceProviders | undefined>,
-  ) => void;
+  setInsurance: (value: React.SetStateAction<InsuranceProviders>) => void;
 }) {
   return (
     <select
       className="select select-bordered w-full text-base"
       defaultValue={"Pick one"}
       onChange={(sel) =>
-        props.setInsurance(sel.target.value as InsuranceProviders | undefined)
+        props.setInsurance(
+          (sel.target.value === "Any"
+            ? undefined
+            : sel.target.value) as InsuranceProviders,
+        )
       }
     >
       <option disabled>Pick one</option>
       <option value={"FQHC"}>Uninsured</option>
       <option value={"QI"}>Med-QUEST</option>
-      <option value={undefined}>Any</option>
+      <option>Any</option>
+    </select>
+  );
+}
+
+function SelectProcedure(props: {
+  setProcedure: (value: React.SetStateAction<ProcedureProviders>) => void;
+}) {
+  return (
+    <select
+      className="select select-bordered w-full text-base"
+      defaultValue={"Pick one"}
+      onChange={(sel) =>
+        props.setProcedure(
+          (sel.target.value === "Any"
+            ? undefined
+            : sel.target.value) as ProcedureProviders,
+        )
+      }
+    >
+      <option disabled>Pick one</option>
+      <option value={"Comprehensive Care"}>Comprehensive Care</option>
+      <option value={"Obstetrician and Gynecologist"}>
+        Obstetrician and Gynecologist
+      </option>
+      <option value={"Internal Medicine"}>Internal Medicine</option>
+      <option value={"RN Family Nurse Practitioner"}>
+        RN Family Nurse Practitioner
+      </option>
+      <option value={"Family Medicine"}>Family Medicine</option>
+      <option value={"Pediatrician"}>Pediatrician</option>
+      <option value={"Podiatrist"}>Podiatrist</option>
+      <option value={"Nephrologist"}>Nephrologist</option>
+      <option>Any</option>
     </select>
   );
 }
@@ -146,6 +242,9 @@ function ClinicResults(props: {
     website: string | null;
     healthCenterNumbers: string[];
     supportedInsurances: string[];
+    procedureTypeNames: string[];
+    procedureTypeIDs: string[];
+    doctorIDs: string[];
   }[];
 }) {
   return props.centers.map((c) => (
@@ -156,7 +255,7 @@ function ClinicResults(props: {
       key={c.id}
     >
       <div className="l-20 text-2xl font-semibold">
-        <a
+        <Link
           href={"/location/" + c.id}
           className={
             c.supportedInsurances.includes("QI")
@@ -165,12 +264,11 @@ function ClinicResults(props: {
           }
         >
           {c.names[0] + " (" + (false || "Comprehensive Care") + ")"}
-        </a>
+        </Link>
       </div>
 
       <div className="flex">
         <div className="min-w-full break-words text-justify">
-          {/* {isLoggedIn ? 'currently' : 'not'} */}
           {c.website && (
             <a
               className="italic text-blue-700 underline"
@@ -196,9 +294,16 @@ function ClinicResults(props: {
             </div>
           )}
           <div>
-            {c.supportedInsurances.includes("QI") && "Quest Insured"}
-            {c.supportedInsurances.includes("FQHC") &&
-              "Federally Qualified Health Center"}
+            {c.supportedInsurances
+              .map((ins) =>
+                ins === "QI"
+                  ? "Quest Insured"
+                  : ins === "FQHC"
+                  ? "Federally Qualified Health Center"
+                  : "",
+              )
+              .sort()
+              .join(" & ")}
           </div>
         </div>
       </div>
--- src/server/api/routers/HealthcareRouter.ts
+++ src/server/api/routers/HealthcareRouter.ts
@@ -3,10 +3,26 @@ import { createTRPCRouter, publicProcedure } from "../trpc";
 import { z } from "zod";
 import type { Prisma } from "@prisma/client";
 
-const InsuranceValidator = z.union([z.literal("FQHC"), z.literal("QI")]);
+const InsuranceValidator = z.union([
+  z.literal("FQHC"),
+  z.literal("QI"),
+  z.undefined(),
+]);
+const ProcedureValidator = z.union([
+  z.literal("Comprehensive Care"),
+  z.literal("Obstetrician and Gynecologist"),
+  z.literal("Internal Medicine"),
+  z.literal("RN Family Nurse Practitioner"),
+  z.literal("Family Medicine"),
+  z.literal("Pediatrician"),
+  z.literal("Podiatrist"),
+  z.literal("Nephrologist"),
+  z.undefined(),
+]);
 
 export type InsuranceProviders = z.infer<typeof InsuranceValidator>;
-export type IDToMeta = Record<string, { names: string[], address: string }>;
+export type ProcedureProviders = z.infer<typeof ProcedureValidator>;
+export type IDToMeta = Record<string, { names: string[]; address: string }>;
 
 const HealthcareRouter = createTRPCRouter({
   getById: publicProcedure
@@ -24,17 +40,21 @@ const HealthcareRouter = createTRPCRouter({
     }),
   // must be FQHC or QI for 'quest insurance'
   getByPlan: publicProcedure
-    .input(z.object({ insurance: InsuranceValidator }))
+    .input(
+      z.object({
+        insurance: InsuranceValidator,
+        procedure: ProcedureValidator,
+      }),
+    )
     .query(async ({ input }) => {
       const a = await db.healthCenter.findMany({
         where: {
-          supportedInsurances: {
-            has: input.insurance,
-          },
-        },
-        take: 100,
+          supportedInsurances: (input.insurance ? { has: input.insurance } : { isEmpty: false }),
+          AND: {
+            procedureTypeNames: (input.procedure ? { has: input.procedure } : { isEmpty: false })
+          }
+        }
       });
-
       return a;
     }),
   getSome: publicProcedure.query(async () => {
@@ -99,24 +119,33 @@ const HealthcareRouter = createTRPCRouter({
     });
     return features;
   }),
-  getDataByName: publicProcedure.input(z.object({ name: z.string() })).query(async ({ input }) => {
-    const lol: ({_id: {$oid: string }} & Prisma.HealthCenterGroupByOutputType)[] = await db.healthCenter.findRaw({
-        filter: {
+  getDataByName: publicProcedure
+    .input(z.object({ name: z.string() }))
+    .query(async ({ input }) => {
+      const lol: ({
+        _id: { $oid: string };
+      } & Prisma.HealthCenterGroupByOutputType)[] =
+        (await db.healthCenter.findRaw({
+          filter: {
             names: {
-                // prisma doesnt have an elegant way of filtering if a letter occurs in a string[] so we use regexes.
-                $regex: input.name.replace(/[/\-\\^$*+?.()|[\]{}]/g, '\\$&'),
-                $options: 'i'
-            }
-        }
-    }) as unknown as [];
-    const format:IDToMeta = {};
-    for(const place of lol)
-      format[place._id.$oid] = { names: place.names, address: place.address }; 
-    return format;
-  }),
-  getAllProcedureTypes: publicProcedure.query(async () => {
-    const t = await db.procedureType.findMany();
-    return t.filter((item,index,self) => self.findIndex(e=>item.name === e.name) === index);
+              // prisma doesnt have an elegant way of filtering if a letter occurs in a string[] so we use regexes.
+              $regex: input.name.replace(/[/\-\\^$*+?.()|[\]{}]/g, "\\$&"),
+              $options: "i",
+            },
+          },
+        })) as unknown as [];
+      const format: IDToMeta = {};
+      for (const place of lol)
+        format[place._id.$oid] = { names: place.names, address: place.address };
+      return format;
+    }),
+  getAllProcedureTypes: publicProcedure.query(() => {
+    return [];
+    //const t = await db.procedureType.findMany();
+    //return t.filter(
+    //  (item, index, self) =>
+    //    self.findIndex((e) => item.name === e.name) === index,
+    //);
   }),
 });
 
